30f0cfceca543e039eb56647a630d802529af90b
Jonathan Sue <hello@jonosue.com>
2017-08-29 19:05:59 +0000

    Added solution for the 3rd exercise to the corresponding file.

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -1,4 +1,4 @@
-require_relative '../setup'
+  require_relative '../setup'
 require_relative './exercise_1'
 require_relative './exercise_2'
 
@@ -6,3 +6,7 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+@store3 = Store.find(3)
+@store3.destroy
+
+puts "There are " + Store.count.to_s + " stores."
